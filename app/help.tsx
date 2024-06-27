a35ff6a8584a38b9ccf09c257c1bb8e4fa8d2c1f
Shawn Dreifuss <shawndreifuss@icloud.com>
2024-06-27 09:37:27 -0700

    project initial setup

---
+++ app/help.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const Help = () => {
+  return (
+    <View>
+      <Text>Help</Text>
+    </View>
+  )
+}
+
+export default Help
---
+++ app/signup.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import { defaultStyles } from '@/constants/Styles'
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <View style={defaultStyles.container}>
+      <Text>Signup</Text>
+    </View>
+  )
+}
+
+export default Signup
